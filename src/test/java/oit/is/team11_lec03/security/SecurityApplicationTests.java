a3c4207b00cf254ede505d68ecef89146d13236b
e1b19044 <e1b19044@oit.ac.jp>
2021-10-12 14:10:53 +0900

    初期コミット

---
+++ src/test/java/oit/is/team11_lec03/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team11_lec03.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
