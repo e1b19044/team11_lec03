0dc378baca9574472d050fd347801a4dbe1d4650
e1b19110 <e1b19110@oit.ac.jp>
2021-10-12 14:48:20 +0900

    index.html

---
+++ src/main/java/oit/is/team11_lec03/security/security/Sample3AuthConfiguration.java
@@ -0,0 +1,46 @@
+package oit.is.team11_lec03.security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class Sample3AuthConfiguration extends WebSecurityConfigurerAdapter {
+
+  /**
+   * 認証処理に関する設定（誰がログインできるか）
+   */
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+
+    auth.inMemoryAuthentication().withUser("note").password(passwordEncoder().encode("pen")).roles("USER");
+
+    
+    auth.inMemoryAuthentication().withUser("apple").password(passwordEncoder().encode("ringo")).roles("ADMIN");
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  /**
+   * 認可処理に関する設定（認証されたユーザがどこにアクセスできるか）
+   */
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+   
+    http.formLogin();
+ 
+    http.authorizeRequests().antMatchers("/").authenticated();
+  
+    http.logout().logoutSuccessUrl("/");
+  }
+}
